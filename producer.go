1b897264736e8ce1f615b66b6d2cb04576aef653
walterBishop <walterkangluo@gmail.com>
2018-08-23 17:47:02 +0800

    feature# Remove policy package
    1. Since producer just make a block, we need not to define policy for
    it.
    2. Remove all code that relate to policy.

--- producer.go
+++ producer.go
@@ -2,34 +2,54 @@ package producer
 
 import (
 	"fmt"
-	justitia_c "github.com/DSiSc/justitia/config"
 	"github.com/DSiSc/ledger"
-	"github.com/DSiSc/producer/config"
-	"github.com/DSiSc/producer/policy"
+	"github.com/DSiSc/producer/common"
 	"github.com/DSiSc/txpool"
+	txpool_c "github.com/DSiSc/txpool/common"
 	"github.com/DSiSc/txpool/common/log"
+	"time"
 )
 
-type Producer interface {
-	Start() error
-	Stop() error
+type Producer struct {
+	txpool *txpool.TxPool
+	time   uint64
+	ledger *ledger.Ledger
 }
 
-func NewProducer(conf *config.ProducerConfig, txpool *txpool.TxPool, ledger *ledger.Ledger) (Producer, error) {
-	var err error
-	var producer Producer
-	producerPolicy := conf.PolicyName
-	switch producerPolicy {
-	case justitia_c.PRODUCER_TIMER:
-		log.Info("Producer policy is timer.")
-		time := conf.PolicyContext.Timer
-		if 0 >= time {
-			log.Error("Time section of timer is invalid.")
-			return nil, fmt.Errorf("Time section of timer is invalid.")
-		}
-		producer, err = policy.NewTimerProducer(time, txpool, ledger)
-	default:
-		log.Error("Now, we only support timer policy producer.")
+func NewProducer(pool *txpool.TxPool, ledger *ledger.Ledger) (*Producer, error) {
+	return &Producer{
+		txpool: pool,
+		ledger: ledger,
+	}, nil
+}
+
+func (self *Producer) MakeBlock() (*common.Block, error) {
+	block, err := self.assembleBlock()
+	if nil != err {
+		log.Error("Assemble block failed.")
+		return nil, fmt.Errorf("AssembleBlock failed.")
 	}
-	return producer, err
+	return block, nil
+}
+
+func (self *Producer) assembleBlock() (*common.Block, error) {
+	txs := self.txpool.GetTxs()
+	txHash := make([]txpool_c.Hash, 0, len(txs))
+	for _, t := range txs {
+		txHash = append(txHash, t.Hash())
+	}
+	txRoot := common.ComputeMerkleRoot(txHash)
+	header := &common.Header{
+		Version:          0,
+		TransactionsRoot: txRoot,
+		Timestamp:        uint32(time.Now().Unix()),
+		Height:           1,
+	}
+
+	block := &common.Block{
+		Header:       header,
+		Transactions: txs,
+	}
+
+	return block, nil
 }
--- producer_test.go
+++ producer_test.go
@@ -1,36 +1,26 @@
 package producer
 
 import (
-	"github.com/DSiSc/producer/config"
 	"github.com/stretchr/testify/assert"
-	"reflect"
 	"testing"
 )
 
-func mock_NewProducerConfig(time uint, num uint) config.ProducerConfig {
-	return config.ProducerConfig{
-		PolicyName: "timer",
-		PolicyContext: config.ProducerPolicy{
-			Timer: uint64(time),
-			Num:   uint64(num),
-		},
-	}
-}
-
 func Test_NewProducer(t *testing.T) {
 	assert := assert.New(t)
-	conf := mock_NewProducerConfig(10, 0)
 
-	producer, err := NewProducer(&conf, nil, nil)
+	producer, err := NewProducer(nil, nil)
 	assert.Nil(err)
+	assert.NotNil(producer)
+}
 
-	p := reflect.TypeOf(producer)
-	method, exist := p.MethodByName("Start")
-	assert.NotNil(method)
-	assert.True(exist)
+func Test_MakeBlock(t *testing.T) {
+	assert := assert.New(t)
+	producer, _ := NewProducer(nil, nil)
+	assert.Panics(func() { producer.MakeBlock() })
+}
 
-	method, exist = p.MethodByName("Stop")
-	assert.NotNil(method)
-	assert.True(exist)
-
+func Test_assembleBlock(t *testing.T) {
+	assert := assert.New(t)
+	producer, _ := NewProducer(nil, nil)
+	assert.Panics(func() { producer.assembleBlock() })
 }
