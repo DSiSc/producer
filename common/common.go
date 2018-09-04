6257b240222c8d0095b06539e5797fe3eaedb000
walterBishop <walterkangluo@gmail.com>
2018-09-04 20:59:24 +0800

    optimise# HeaderHash() optimise
    1. Avoid compute header hash repeat once it has exist.

--- common/common.go
+++ common/common.go
@@ -26,7 +26,12 @@ func BlockHash(block *types.Block) (hash types.Hash) {
 	copy(hash[:], sumByte)
 	return
 }
+
 func HeaderHash(block *types.Block) (hash types.Hash) {
+	if *(new(types.Hash)) != block.HeaderHash {
+		hash = block.HeaderHash
+		return
+	}
 	header := block.Header
 	jsonByte, _ := json.Marshal(header)
 	sumByte := Sum(jsonByte)
